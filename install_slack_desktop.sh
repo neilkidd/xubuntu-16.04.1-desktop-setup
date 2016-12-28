7b201905efefaf1a63260925772dc1aca5e9ecaf
neilkidd <neil.a.kidd@gmail.com>
2016-12-28 14:22:09 +0000

    Updated slack desktop to 2.3.4

--- install_slack_desktop.sh
+++ install_slack_desktop.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 function install_slack_desktop {
-  
+
   REPO_CHECK=$(dpkg-query -W --showformat='${Status}\n' slack-desktop | grep "install ok installed")
   if [ "" == "$REPO_CHECK" ]; then
-    curl -O https://slack-ssb-updates.global.ssl.fastly.net/linux_releases/slack-desktop-2.0.3-amd64.deb
+    curl -O https://downloads.slack-edge.com/linux_releases/slack-desktop-2.3.4-amd64.deb
     dpkg -i slack-desktop-*.deb
   fi
   apt_install slack-desktop
   # Cleanup and finish
   rm slack-desktop-*.deb
-  
-}
 
+}
