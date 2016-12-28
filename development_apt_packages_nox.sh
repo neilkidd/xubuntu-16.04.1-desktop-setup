bfcca2fde4a5d1d8f3f832d9a9d8896dc1b4f4b9
neilkidd <neil.a.kidd@gmail.com>
2016-12-28 20:47:16 +0000

    Added ncdu se: https://heitorlessa.com/ncdu-different-way-to-obtain-disk-usage-in-linux-a0ac2f9270c9#.2ipo5lpjc

--- development_apt_packages_nox.sh
+++ development_apt_packages_nox.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
 function install_development_apt_packages_nox {
-  
+
   declare -a DEVELOPMENT_APT_PACKAGES_NOX=(
     ack-grep
     htop
     libav-tools
     mediainfo
+    ncdu
     nmap
     tmux
     tree
     vim
   )
-  
+
   for element in ${DEVELOPMENT_APT_PACKAGES_NOX[@]}; do
     apt_install $element;
   done
-  
+
   unset DEVELOPMENT_APT_PACKAGES_NOX;
 }
-
-
-
