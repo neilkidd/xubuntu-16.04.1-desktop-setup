f3fba28033ce53204788d883d4652f46a47d3d71
neilkidd <neil.a.kidd@gmail.com>
2016-12-28 14:46:24 +0000

    Explicity install ttf-mscorefonts-installer

--- install_xubuntu_restricted_extras.sh
+++ install_xubuntu_restricted_extras.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 function install_xubuntu_restricted_extras {
-  
+
  debconf-set-selections <<< 'ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true'
+ apt_install ttf-mscorefonts-installer
  apt_install xubuntu-restricted-extras
 
 }
-
